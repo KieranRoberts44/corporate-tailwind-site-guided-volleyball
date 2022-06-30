8b73809eb26fe8301a810a258fa064fa60a8ee37
KieranRoberts <kieran-roberts@live.com>
2022-06-30 21:20:57 +1200

    First commit

---
+++ pages/contact.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { getProps } from "@builtjs/site";
+import Error from "next/error";
+import { withRouter } from "next/router";
+import SEO from "@/templates/head/seo/seo";
+import Block1 from "@/templates/blocks/block-1/block-1";
+import Form1 from "@/templates/forms/form-1/form-1";
+import Layout from "@/layout/layout";
+
+const Page = (props) => {
+  if (props.error) {
+    return <Error statusCode={props.error.code} />
+  }
+  return (
+     <>
+	<SEO content={props.contactSeoContent} />
+		<Layout headerContent={props.headerContent}  footerContent={props.footerContent} >
+			 <Form1 content={props.contactLandingContent} />
+			 <Block1 content={props.aboutTeaserContent} />
+			</Layout>
+		</>
+  );
+};
+
+export default withRouter(Page);
+
+export async function getStaticProps() {
+  let props = await getProps({pageSlug: 'contact'});
+  return {
+    props: props
+  };
+}
